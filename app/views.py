1ad87b16d0e424506ee0bba17f04a181423a7a84
Benjamin J. Young <blaher09@gmail.com>
2017-07-12 10:14:18 -0400

    Adding bootstrap

--- app/views.py
+++ app/views.py
@@ -18,16 +18,6 @@ def settings(request):
     user = request.user
 
     try:
-        github_login = user.social_auth.get(provider='github')
-    except UserSocialAuth.DoesNotExist:
-        github_login = None
-
-    try:
-        twitter_login = user.social_auth.get(provider='twitter')
-    except UserSocialAuth.DoesNotExist:
-        twitter_login = None
-
-    try:
         facebook_login = user.social_auth.get(provider='facebook')
     except UserSocialAuth.DoesNotExist:
         facebook_login = None
@@ -35,8 +25,6 @@ def settings(request):
     can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
 
     return render(request, 'core/settings.html', {
-        'github_login': github_login,
-        'twitter_login': twitter_login,
         'facebook_login': facebook_login,
         'can_disconnect': can_disconnect
     })
